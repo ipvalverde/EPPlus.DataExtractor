9cc623662a9c8c5f5037446a36de1290c02337ef
Israel Valverde <israelpereiravalverde@gmail.com>
2017-03-06 00:33:48 +0100

    Readme updated

--- README.md
+++ README.md
@@ -1,2 +1,103 @@
-# EPPlus.DataExtractor
-EPPlus extension that make easier to extract POCO from excel tables
+# Summary
+This is a simple EPPlus extension that make easier to extract POCO from spreadsheets.
+
+## Installation
+You can download the library from nuget with the following command:
+
+´´´
+Install-Package EPPlus.DataExtractor
+´´´
+
+If you don´t have the EPPlus as a dependency, nuget will install it for you.
+
+## Usage
+Using the package is really simple. Let's work with a simple example first.
+
+Consider you want to extract the following data from a spreadsheet:
+
+![First table](images/Table1.png)
+
+Just a define a class to hold the data.
+
+```csharp
+public class SimpleRowData
+{
+    public string CarName { get; set; }
+
+    public double Value { get; set; }
+
+    public DateTime CreationDate { get; set; }
+}
+```
+
+Considering that the data is on a worksheet named **worksheet1**, this is the code that can be used to retrieve an `IEnumerable` of `SimpleRowData`:
+
+```csharp
+using (var package = new ExcelPackage("spreadsheet/file/location/file.xlsx"))
+{
+    var cars = package.Workbook.Worksheets["worksheet1"]
+        .Extract<SimpleRowData>()
+        // Here we can chain multiple definition for the columns
+        .WithProperty(p => p.CarName, "B")
+        .WithProperty(p => p.Value, "C")
+        .WithProperty(p => p.CreationDate, "D")
+        .GetData(4, 6) // To obtain the data we indicate the start and end of the rows.
+        // In this case, the rows 4, 5 and 6 will be extracted.
+        .ToList();
+}
+```
+
+Yeah, it is that simple!
+
+### Columns that should be rows
+Sometimes the tables defined in spreadsheets does not have a friendly structure for a developer. Instead of creating multiple tables and foreign key relationships in excel it is simpler to put data that should go into different tables as columns in the existing table.
+It'll be clear with the following example:
+
+![Table 2](images/Table2.PNG)
+
+Imagine that you want to extract this data into a class structure where the columns that indicates months/year should be a collection inside the entity that will keep the row data. So we can have two classes defined like that:
+
+```csharp
+public class RowDataWithColumnBeingRow
+{
+    public string Name { get; set; }
+
+    public int Age { get; set; }
+
+    public List<ColumnData> MoneyData { get; set; }
+}
+
+public class ColumnData
+{
+    public double ReceivedMoney { get; set; }
+
+    public DateTime Date { get; set; }
+}
+```
+
+You can use the following code to extract the spreadsheet data to these classes:
+
+```csharp
+using (var package = new ExcelPackage("spreadsheet/file/location/file.xlsx"))
+{
+    var data = package.Workbook.Worksheets["worksheet1"]
+        .Extract<RowDataWithColumnBeingRow>()
+        .WithProperty(p => p.Name, "F")
+        .WithProperty(p => p.Age, "G")
+
+        // Here, the collection property is defined using the "WithCollectionProperty" method.
+        // The following parameter is the expression indicating the property of "ColumnData"
+        // that will be used to receive the header data followed by an integer indicating the row
+        // that contains the header.
+        // The last expression indicates the other "ColumnData" property, this one will receive
+        // the row data. The two last strings are the start and end column from where
+        // this data will be extracted.
+        .WithCollectionProperty(p => p.MoneyData,
+            item => item.Date, 1,
+            item => item.ReceivedMoney, "H", "S")
+        .GetData(2, 4)
+        .ToList();
+}
+```
+
+The `GetData` method returns an `IEnumerable`, and this `IEnumerable` is not evaluated until you interate through it or execute something like `ToList` over it. So make sure you'll do one of those things **before disposing the EPPlus `ExcelPackage`**.
---
+++ src/EPPlus.DataExtractor/IDataExtractorInterfaces.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq.Expressions;
+
+namespace EPPlus.DataExtractor
+{
+    /// <summary>
+    /// Instance used to configure the data
+    /// extraction.
+    /// </summary>
+    /// <typeparam name="TRow"></typeparam>
+    public interface IDataExtractor<TRow>
+        where TRow : class, new()
+    {
+        /// <summary>
+        /// Maps a property from the type defined as the row model
+        /// to the column identifier that has its value.
+        /// </summary>
+        /// <typeparam name="TValue"></typeparam>
+        /// <param name="propertyExpression">Expression for the property to be mapped.</param>
+        /// <param name="column">The column that contains the value to be mapped to
+        /// the property defined by <paramref name="propertyExpression"/>.</param>
+        /// <returns></returns>
+        ICollectionPropertyConfiguration<TRow> WithProperty<TValue>(Expression<Func<TRow, TValue>> propertyExpression,
+            string column);
+    }
+
+    public interface IGetData<TRow>
+        where TRow : class, new()
+    {
+        /// <summary>
+        /// Obtains the entities for the rows previously configured.
+        /// </summary>
+        /// <param name="fromRow">The initial row to start the data extraction.</param>
+        /// <param name="toRow">The last row to read the data.</param>
+        /// <returns>Returns an <see cref="IEnumerable{T}"/> with the data of the columns.</returns>
+        IEnumerable<TRow> GetData(int fromRow, int toRow);
+    }
+
+    public interface IConfiguredDataExtractor<TRow> : IDataExtractor<TRow>, IGetData<TRow>
+        where TRow : class, new()
+    {
+    }
+
+    public interface ICollectionPropertyConfiguration<TRow> : IConfiguredDataExtractor<TRow>
+        where TRow : class, new()
+    {
+        /// <summary>
+        /// Configure a collection property from <typeparamref name="TRow"/> object
+        /// that will be populated by columns data, instead of rows.
+        /// </summary>
+        /// <typeparam name="TCollectionItem">The type used inside a collection.
+        /// This type will usually have two properties, one to hold the column header and another
+        /// one for the row value.</typeparam>
+        /// <typeparam name="THeaderValue">The type of the header column.</typeparam>
+        /// <typeparam name="TRowValue">The type of the row value.</typeparam>
+        /// <param name="propertyCollection">An expression that specifies the collection
+        /// property that will hold the values.</param>
+        /// <param name="headerProperty">The expression property from <typeparamref name="TCollectionItem"/>
+        /// indicating the property that will be populated with the header value.</param>
+        /// <param name="headerRow">The row number that contains the header data.</param>
+        /// <param name="rowProperty">>The expression property from <typeparamref name="TCollectionItem"/>
+        /// indicating the property that will be populated with the row value.</param>
+        /// <param name="startColumn">The start of the column that will be extract to the collection.</param>
+        /// <param name="endColumn">The start of the column that will be extract to the collection.</param>
+        /// <returns></returns>
+        ICollectionPropertyConfiguration<TRow> WithCollectionProperty<TCollectionItem, THeaderValue, TRowValue>(
+            Expression<Func<TRow, List<TCollectionItem>>> propertyCollection,
+            Expression<Func<TCollectionItem, THeaderValue>> headerProperty, int headerRow,
+            Expression<Func<TCollectionItem, TRowValue>> rowProperty,
+            string startColumn, string endColumn)
+            where TCollectionItem : class, new();
+
+        /// <summary>
+        /// Configure a collection property from <typeparamref name="TRow"/> object
+        /// that will be populated by columns data, instead of rows.
+        /// </summary>
+        /// <typeparam name="TCollectionItem">The type used inside a collection.
+        /// This type will usually have two properties, one to hold the column header and another
+        /// one for the row value.</typeparam>
+        /// <typeparam name="THeaderValue">The type of the header column.</typeparam>
+        /// <typeparam name="TRowValue">The type of the row value.</typeparam>
+        /// <param name="propertyCollection">An expression that specifies the collection
+        /// property that will hold the values.</param>
+        /// <param name="headerProperty">The expression property from <typeparamref name="TCollectionItem"/>
+        /// indicating the property that will be populated with the header value.</param>
+        /// <param name="headerRow">The row number that contains the header data.</param>
+        /// <param name="rowProperty">>The expression property from <typeparamref name="TCollectionItem"/>
+        /// indicating the property that will be populated with the row value.</param>
+        /// <param name="startColumn">The start of the column that will be extract to the collection.</param>
+        /// <param name="endColumn">The start of the column that will be extract to the collection.</param>
+        /// <returns></returns>
+        ICollectionPropertyConfiguration<TRow> WithCollectionProperty<TCollectionItem, THeaderValue, TRowValue>(
+            Expression<Func<TRow, HashSet<TCollectionItem>>> propertyCollection,
+            Expression<Func<TCollectionItem, THeaderValue>> headerProperty, int headerRow,
+            Expression<Func<TCollectionItem, TRowValue>> rowProperty,
+            string startColumn, string endColumn)
+            where TCollectionItem : class, new();
+
+        /// <summary>
+        /// Configure a collection property from <typeparamref name="TRow"/> object
+        /// that will be populated by columns data, instead of rows.
+        /// </summary>
+        /// <typeparam name="TCollectionItem">The type used inside a collection.
+        /// This type will usually have two properties, one to hold the column header and another
+        /// one for the row value.</typeparam>
+        /// <typeparam name="THeaderValue">The type of the header column.</typeparam>
+        /// <typeparam name="TRowValue">The type of the row value.</typeparam>
+        /// <param name="propertyCollection">An expression that specifies the collection
+        /// property that will hold the values.</param>
+        /// <param name="headerProperty">The expression property from <typeparamref name="TCollectionItem"/>
+        /// indicating the property that will be populated with the header value.</param>
+        /// <param name="headerRow">The row number that contains the header data.</param>
+        /// <param name="rowProperty">>The expression property from <typeparamref name="TCollectionItem"/>
+        /// indicating the property that will be populated with the row value.</param>
+        /// <param name="startColumn">The start of the column that will be extract to the collection.</param>
+        /// <param name="endColumn">The start of the column that will be extract to the collection.</param>
+        /// <returns></returns>
+        ICollectionPropertyConfiguration<TRow> WithCollectionProperty<TCollectionItem, THeaderValue, TRowValue>(
+            Expression<Func<TRow, Collection<TCollectionItem>>> propertyCollection,
+            Expression<Func<TCollectionItem, THeaderValue>> headerProperty, int headerRow,
+            Expression<Func<TCollectionItem, TRowValue>> rowProperty,
+            string startColumn, string endColumn)
+            where TCollectionItem : class, new();
+    }
+}

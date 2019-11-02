079c2e9d8378b11babe4fb225760aa58ad3348e9
Israel Valverde <israelpereiravalverde@gmail.com>
2019-11-02 22:45:14 +0000

    Adds helper method to validate collection property of expression func

--- src/EPPlus.DataExtractor/ExpressionHelper.cs
+++ src/EPPlus.DataExtractor/ExpressionHelper.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq.Expressions;
+using System.Reflection;
 
 namespace EPPlus.DataExtractor
 {
@@ -14,6 +16,47 @@ namespace EPPlus.DataExtractor
                 propertyExpression.Parameters[0], parameter);
 
             return setPropActionExpression.Compile();
+        }
+
+        internal static void ValidatePropertyExpressionType<TModel, TCollectionItem, TCollectionProperty>(Expression<Func<TModel, TCollectionProperty>> collectionPropertyExpr)
+            where TCollectionProperty : class, ICollection<TCollectionItem>, new()
+        {
+            var expectedType = typeof(TCollectionProperty);
+            if (!expectedType.IsGenericType)
+            {
+                throw new ArgumentException($"The collection property must be a generic type. Given type: {expectedType.FullName}");
+            }
+            expectedType = expectedType.GetGenericTypeDefinition();
+
+            var memberExpression = GetMemberExpression();
+            var propertyInfo = memberExpression.Member as PropertyInfo;
+            
+            var propertyType = propertyInfo.PropertyType;
+            if (!propertyType.IsGenericType)
+            {
+                throw new ArgumentException($"The property for the given collection expression must be of generic type {expectedType}.");
+            }
+            var genericCollectionType = propertyType.GetGenericTypeDefinition();
+            if (expectedType != genericCollectionType)
+            {
+                throw new ArgumentException($"The property for the given collection expression must be of type {expectedType.FullName}. Given type: {genericCollectionType.FullName}");
+            }
+
+
+            MemberExpression GetMemberExpression()
+            {
+                switch (collectionPropertyExpr.Body)
+                {
+                    case UnaryExpression unaryExpression:
+                        if (unaryExpression.Operand is MemberExpression)
+                            return (MemberExpression)unaryExpression.Operand;
+                        break;
+                    case MemberExpression mExpression:
+                        return mExpression;
+                }
+
+                throw new ArgumentException();
+            }
         }
     }
 }
